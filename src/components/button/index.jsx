441f3c2a59bf6560697ca91eddd1c0785cc89853
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-07 21:43:03 +0700

    feat: global components button and card

---
+++ src/components/button/index.jsx
@@ -0,0 +1,30 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import React from 'react';
+import { css, jsx } from '@emotion/react';
+
+const styles = {
+  root: css`
+    padding: 12px 32px;
+    background: #495057;
+    color: #ced4da;
+    border-radius: 8px;
+    cursor: pointer;
+    border: 0px;
+
+    &:hover {
+      background: black;
+    }
+  `,
+};
+
+function Button(props) {
+  const { children, ...others } = props;
+  return (
+    <button css={styles.root} type="button" {...others}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/components/cards/home/index.jsx
@@ -0,0 +1,49 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import React from 'react';
+import { css, jsx } from '@emotion/react';
+import Button from '../../button';
+
+const styles = {
+  wrapper: css`
+    border-radius: 8px;
+    color: white;
+    background: #343a40;
+    height: 100%;
+  `,
+  content_wrapper: css`
+    padding: 16px;
+  `,
+  img_wrapper: css`
+    height: 160px;
+    width: 100%;
+    border-radius: 8px 8px 0px 0px;
+    overflow: hidden;
+    z-index: -1;
+  `,
+  img: css`
+    object-fit: cover;
+    object-position: center;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100%;
+    border-radius: 8px 8px 0px 0px;
+  `,
+};
+
+function CardHome({ title, description, imgUrl, ...others }) {
+  return (
+    <div css={styles.wrapper} {...others}>
+      <div css={styles.img_wrapper}>
+        <img css={styles.img} src={imgUrl} alt="test-1" />
+      </div>
+      <div css={styles.content_wrapper}>
+        <h3 className="mb-2">{title}</h3>
+        <p className="regular-metadata-xs mb-3">{description}</p>
+        <Button>Try this!</Button>
+      </div>
+    </div>
+  );
+}
+
+export default CardHome;
