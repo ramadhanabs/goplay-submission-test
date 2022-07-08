42491ae160c997c28112756738364863c1cbbff5
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-08 13:24:25 +0700

    refactor: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import './assets/css/typography.css';
 import './assets/css/spacing.css';
 import Home from './pages/home';
+import SimpleColorPicker from './pages/simple-color-picker';
+import ImagesContainer from './pages/images-container';
+import NumberToText from './pages/number-to-text';
 
-/* Refactor route to separate file */
-const App = () =>
-  useRoutes([
-    {
-      path: '/',
-      element: <Home />,
-    },
-  ]);
-
-function AppWrapper() {
+function App() {
   return (
-    <Router>
-      <main>
-        <App />
-      </main>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/simple-color-picker" element={<SimpleColorPicker />} />
+        <Route path="/images-container" element={<ImagesContainer />} />
+        <Route path="/number-to-text" element={<NumberToText />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default AppWrapper;
+export default App;
