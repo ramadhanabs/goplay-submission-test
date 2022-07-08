85853a386b134c2d3bb2d43f7d93240dd8216775
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-08 17:30:23 +0700

    feat: add simple color picker pages

---
+++ src/pages/simple-color-picker/index.jsx
@@ -0,0 +1,30 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import AppLayout from '../../components/app-layout';
+import SimpleColorPickerApp from '../../apps/simple-color-picker-app';
+
+const styles = {
+  wrapper: css`
+    padding: 16px;
+    border-radius: 16px;
+    background: #343a40;
+    width: auto;
+    color: white;
+  `,
+};
+
+function SimpleColorPicker() {
+  return (
+    <AppLayout>
+      <div className="mb-3" css={styles.wrapper}>
+        <h3>Simple Color Picker</h3>
+      </div>
+      <div css={styles.wrapper}>
+        <SimpleColorPickerApp />
+      </div>
+    </AppLayout>
+  );
+}
+
+export default SimpleColorPicker;
