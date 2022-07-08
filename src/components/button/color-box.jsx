244502cc9c9e934fd88f07e5f5c000208627b49e
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-08 17:29:01 +0700

    feat: color box button component

---
+++ src/components/button/color-box.jsx
@@ -0,0 +1,45 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+
+const styles = {
+  root: css`
+    width: 100%;
+    height: 30px;
+    border-radius: 4px;
+    margin: 4px;
+    cursor: pointer;
+    border: none;
+    opacity: 0.8;
+    transition: all 0.3s ease;
+
+    &:hover {
+      transform: scale(1.1);
+      opacity: 1;
+      transition: all 0.3s ease;
+    }
+  `,
+  active: css`
+    border: 2px solid yellow;
+    transform: scale(1.1);
+    opacity: 1;
+    transition: all 0.3s ease;
+  `,
+};
+
+function ColorBox({ isActive, color, ...others }) {
+  return (
+    <button
+      type="button"
+      key={color}
+      css={css`
+        ${styles.root}
+        ${isActive && styles.active}
+      `}
+      style={{ background: color }}
+      {...others}
+    />
+  );
+}
+
+export default ColorBox;
