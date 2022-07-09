81627889db14ad2416a6cf37ceb6e56f41727ec5
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-09 21:46:30 +0700

    feat: add loading component

---
+++ src/components/loading/index.jsx
@@ -0,0 +1,28 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import LoadingGIF from '../../assets/images/loading.gif';
+
+const styles = {
+  root: css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 400px;
+  `,
+  img: css`
+    width: 48px;
+    height: 48px;
+  `,
+};
+
+function Loading() {
+  return (
+    <div css={styles.root}>
+      <img src={LoadingGIF} css={styles.img} alt="" />
+      <h3 className="ml-2">Loading</h3>
+    </div>
+  );
+}
+
+export default Loading;
