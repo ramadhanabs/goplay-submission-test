eac5180a1a06c88d526b3c8f344d54926dda7cbf
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-09 19:45:12 +0700

    feat: add input component

---
+++ src/components/input/index.jsx
@@ -0,0 +1,33 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+
+const styles = {
+  root: css`
+    padding: 12px 16px;
+    min-width: 200px;
+    border: none;
+    background: #495056;
+    border-radius: 8px;
+    color: white;
+    outline: none;
+    &:focus {
+      border: 1px solid #8066b5;
+      background: #424a52;
+    }
+    ::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    ::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+  `,
+};
+
+function Input({ ...others }) {
+  return <input css={styles.root} {...others} />;
+}
+
+export default Input;
