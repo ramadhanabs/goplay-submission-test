7b5a31b3fb396358e404a4fe4766297431d93bf2
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-11 06:43:03 +0700

    style: fix layouting

--- src/apps/number-to-text-app/index.jsx
+++ src/apps/number-to-text-app/index.jsx
@@ -6,8 +6,13 @@ function NumberToTextApp() {
   const [input, setInput] = useState(0);
   const [text, setText] = useState('');
 
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
   useEffect(() => {
     const convertText = () => {
+      if (input.length > 15) return;
       const str = numberToText(input);
       setText(str);
     };
@@ -16,20 +21,32 @@ function NumberToTextApp() {
   }, [input]);
   return (
     <div>
-      <div className="mb-3">
-        <p className="regular-metadata mb-2">Input your number here:</p>
-        <Input
-          type="number"
-          placeholder="Input your number"
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
-        />
-      </div>
+      <div className="row mb-3">
+        <div className="col-6 mr-4">
+          <div className="mb-4">
+            <p className="regular-metadata mb-2">Number:</p>
+            <Input
+              type="number"
+              placeholder="Input your number"
+              onChange={handleInput}
+              block
+            />
+          </div>
 
-      <div>
-        <p className="regular-metadata mb-2">Your converted text:</p>
-        <p className="bold-body">{text || '--'}</p>
+          <div>
+            <p className="bold-metadata mb-2">How to use:</p>
+            <p className="regular-metadata">
+              You can simply convert from number to words. But remember this
+              converter still has limitation, you can only input maximum
+              15-digit number. So, make sure your input is no longer than
+              15-digit 😁
+            </p>
+          </div>
+        </div>
+        <div className="col-6">
+          <p className="regular-metadata mb-2">Converted Text:</p>
+          <p className="bold-body">{text || '--'}</p>
+        </div>
       </div>
     </div>
   );
