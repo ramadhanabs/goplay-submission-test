9b13f418d89181c8c04731ec1d0b8139b721cafa
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-11 06:47:13 +0700

    refactor: styling

--- src/pages/simple-color-picker/index.jsx
+++ src/pages/simple-color-picker/index.jsx
@@ -4,21 +4,11 @@ import { css, jsx } from '@emotion/react';
 import AppLayout from '../../components/app-layout';
 import SimpleColorPickerApp from '../../apps/simple-color-picker-app';
 
-const styles = {
-  wrapper: css`
-    padding: 16px;
-    border-radius: 16px;
-    background: #343a40;
-    width: auto;
-    color: white;
-  `,
-};
-
 function SimpleColorPicker() {
   return (
     <AppLayout>
-      <div className="mb-3" css={styles.wrapper}>
-        <h3>Simple Color Picker</h3>
+      <div className="mb-3">
+        <h2>Simple Color Picker</h2>
       </div>
       <div>
         <SimpleColorPickerApp />
