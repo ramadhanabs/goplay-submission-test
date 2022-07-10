8e54209ac94ac28fa5aa42d5fe1dc3ba9ef71cc5
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-11 06:46:44 +0700

    refactor: delete unused styles

--- src/pages/images-container/index.jsx
+++ src/pages/images-container/index.jsx
@@ -4,16 +4,6 @@ import { css, jsx } from '@emotion/react';
 import AppLayout from '../../components/app-layout';
 import ImagesContainerApp from '../../apps/images-container-app';
 
-const styles = {
-  wrapper: css`
-    padding: 16px;
-    border-radius: 16px;
-    background: #343a40;
-    width: auto;
-    color: white;
-  `,
-};
-
 function ImagesContainer() {
   return (
     <AppLayout>
