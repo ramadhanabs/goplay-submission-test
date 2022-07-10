4f434c978cccc12c4105c5eb572f5e3aa0cba2a0
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-11 06:45:03 +0700

    feat: add pagination dots

---
+++ src/components/pagination-dots/index.jsx
@@ -0,0 +1,34 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+
+const styles = {
+  wrapper: css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  root: css`
+    width: 10px;
+    height: 10px;
+    border-radius: 10px;
+    background: white;
+    opacity: 0.1;
+    margin: 0px 4px;
+  `,
+  active: css`
+    opacity: 1;
+  `,
+};
+
+function PaginationDots({ isActive, count }) {
+  return (
+    <div css={styles.wrapper}>
+      {count.map((o, index) => (
+        <div css={[styles.root, isActive === index + 1 && styles.active]} />
+      ))}
+    </div>
+  );
+}
+
+export default PaginationDots;
