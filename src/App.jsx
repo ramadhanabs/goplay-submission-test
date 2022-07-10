27322e6f0efb16c10d2734c0d1fdca067eccf789
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-11 06:40:55 +0700

    feat: add context of display mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import Home from './pages/home';
 import SimpleColorPicker from './pages/simple-color-picker';
 import ImagesContainer from './pages/images-container';
 import NumberToText from './pages/number-to-text';
+import { DisplayProvider } from './lib/context/display-context';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/simple-color-picker" element={<SimpleColorPicker />} />
-        <Route path="/images-container" element={<ImagesContainer />} />
-        <Route path="/number-to-text" element={<NumberToText />} />
-      </Routes>
+      <DisplayProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/simple-color-picker" element={<SimpleColorPicker />} />
+          <Route path="/images-container" element={<ImagesContainer />} />
+          <Route path="/number-to-text" element={<NumberToText />} />
+        </Routes>
+      </DisplayProvider>
     </BrowserRouter>
   );
 }
