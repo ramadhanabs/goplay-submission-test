00da9c3698e02f725d4876fc72d37a9d1979529d
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-07-12 14:51:59 +0700

    test: function helper

---
+++ src/lib/helper/__test__/index.test.js
@@ -0,0 +1,37 @@
+import { describe, it, expect } from 'vitest';
+import {
+  arrayOfColor,
+  arrayToCollection,
+  generateArrayofColor,
+} from '../colorGenerator';
+import { numberToText } from '../numberToText';
+
+describe('Color generator function test', () => {
+  const array = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+  it('should return exact 512 color length', () => {
+    expect(arrayOfColor(512)).toHaveLength(512);
+  });
+
+  it('#', () => {
+    const obj = arrayToCollection(array, 2);
+    const arrayOfKeys = Object.keys(obj);
+    expect(arrayOfKeys).toHaveLength(3);
+  });
+
+  it('#', () => {
+    const obj = generateArrayofColor();
+    const arrayOfKeys = Object.keys(obj);
+
+    expect(arrayOfKeys).toHaveLength(8);
+  });
+});
+
+describe('Number to text function test', () => {
+  it('should return nothing when no input', () => {
+    expect(numberToText()).toBe(undefined);
+  });
+  it('should return converted number to words', () => {
+    expect(numberToText(1000)).toBe('one thousand');
+  });
+});
